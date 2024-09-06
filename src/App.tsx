2169c38466c0cf5e542b85329ab29ba135482a25
theskyworld <2456450687@qq.com>
2024-09-06 15:56:01 +0800

    chore: 添加tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
+    <div className="text-3xl font-bold underline text-red-400">
       <h1>hello YouYiGO</h1>
-   </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
