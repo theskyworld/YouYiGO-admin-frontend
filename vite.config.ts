b3d2a63c43dab4a09f262afd6ac604a41611ce86
theskyworld <2456450687@qq.com>
2024-09-08 22:40:54 +0800

    chore: 配置开发环境的host

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    host:
+      mode === "production"
+        ? loadEnv("production", process.cwd()).VITE_APP_HOST
+        : "localhost",
+  },
+}));
